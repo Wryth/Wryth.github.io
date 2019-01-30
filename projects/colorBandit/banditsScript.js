6cc563880a62959da4b52afe655979e92690b95c
DESKTOP-834203O\Viktor <vikhan91@gmail.com>
2019-01-30 03:20:42 +0100

    new version of the Colorbandit

--- projects/colorBandit/banditsScript.js
+++ projects/colorBandit/banditsScript.js
@@ -11,6 +11,7 @@ var loss = new Array(1,1,1);
 var bSample = [0,0,0];
 var bMean = [0,0,0];
 var palette = [];
+var shadesOfSample = [[0,0,0],[0,0,0],[0,0,0]];
 
 function betaMean(wlist,llist) {
   var meanColor = wlist.map(
@@ -33,15 +34,15 @@ function betaSampler(wlist,llist){
 
 // move distribution mean towards sample.
 function success() {
-    wins = wins.map((x, i) => x + praisAndBlame(bMean,bSample)[i]);
-    loss = loss.map((x, i) => x + praisAndBlame(bSample,bMean)[i]);
-    select();
+  wins = wins.map((x, i) => x + praisAndBlame(bMean,bSample)[i]);
+  loss = loss.map((x, i) => x + praisAndBlame(bSample,bMean)[i]);
+  select();
 }
 
 // move distribution mean away from sample.
 function fail() {
-  loss = loss.map((x, i) => x + praisAndBlame(bMean,bSample)[i]);
   wins = wins.map((x, i) => x + praisAndBlame(bSample,bMean)[i]);
+  loss = loss.map((x, i) => x + praisAndBlame(bMean,bSample)[i]);
   select();
 }
 
@@ -49,6 +50,7 @@ function fail() {
 function select() {
   bSample = betaSampler(wins,loss);
   bMean = betaMean(wins,loss);
+  shadesOfSample = generateShades([bSample,bSample,bSample,bSample,bSample],0.8,0.1);
   var meanrgbs = bMean.map(x => Math.floor(x*250));
   var rgbs = bSample.map(x => Math.floor(x*250));
   setColors(meanrgbs, rgbs);
@@ -91,9 +93,12 @@ function colorIsEqual(arr) {
   return result;
 }
 
-// move the current collor to the pallet.
+
+//eventListener spesific Functions:
+
+// move the current color to the palette.
 function saveColor() {
-  var rgbColor = bSample.map(x => Math.floor(x*250));
+  var rgbColor = betaToRgb(bSample);
   if (!colorIsEqual(rgbColor)) {
     palette.push(rgbColor);
   };
@@ -101,12 +106,11 @@ function saveColor() {
   showPalette();
 }
 
-// add each pallet color to an html associated element.
+// add each palette color to an html associated element.
 function showPalette(){
   for (let i = 0; i < palette.length; i++) {
     let id = "palettePos" + i;
     document.getElementById(id).style.backgroundColor = "rgb(" + palette[i][0] + "," + palette[i][1] + "," + palette[i][2] + ")";
-    //document.getElementById(id).innerText = "rgb(" + palette[i][0] + "," + palette[i][1] + "," + palette[i][2] + ")"; // Text does not scale right yet.
   }
 }
 
@@ -117,6 +121,73 @@ function resetBeta() {
   select();
 }
 
+// toggles the shadeBox visability.
+function toggleShades() {
+  var boxStyle = "none";
+  if (document.getElementById("shadesBox").style.display === "none") {
+    boxStyle = "block";
+  }
+  document.getElementById("shadesBox").style.display = boxStyle;
+  setShades();
+}
+
+// generate sample color shades,
+function setShades() {
+  console.log(shadesOfSample);
+  for (let i = 0; i < shadesOfSample.length; i++) {
+    var shadeId = "shade" + i;
+    var rgbs = betaToRgb(shadesOfSample[i]); 
+    document.getElementById(shadeId).style.backgroundColor = "rgb(" + rgbs[0] + "," + rgbs[1] + "," + rgbs[2] + ")";
+  }
+}
+
+//Close shades, and apply the pick as the new sample color.
+function moveToSample(event) {
+  var targetElement = event.target;
+  var colorId = targetElement.id; // get current button.
+  var idSplit = colorId.split("e"); // split by shad"e" [0-5]
+  bSample = shadesOfSample[idSplit[1]]; // set new bSample.
+  setColors(betaToRgb(bMean),betaToRgb(bSample));
+  toggleShades();
+}
+
+//create shades of Beta
+function shadesOfBeta(sample, prosentage) {
+  var shade = sample.map(x => x * prosentage);
+  return shade;
+}
+
+// create an increment of shades.(pr = start pr, incrementas are by how much each shade increases.)
+function generateShades(shadesArr,pr,increments) {
+  for (let i = 0; i < shadesArr.length; i++) {
+    shadesArr[i] = shadesOfBeta(shadesArr[i],pr)
+    pr = pr + increments;
+  }
+  return shadesArr;
+}
+
+// Converts the betasamples in to a rgb number.
+function betaToRgb(betaArr) {
+  return betaArr.map(x => Math.floor(x*250));
+}
+
+// Sets an eventlisner to the given class
+function setClassEventListener(aclassname, atype, afunction) {
+  var thisClass = document.getElementsByClassName(aclassname);
+  for (var i = 0; i < thisClass.length; i++) {
+    thisClass[i].addEventListener(atype, afunction, false);
+  }
+}
+
+function exportPalette() {
+  var alertString = "";
+  for (let i = 0; i < palette.length; i++) {
+    alertString = alertString + "rgb(" + palette[i] + ")\n";
+  }
+  alert(alertString);
+}
+
+
 // EventListeners
 window.onload = function (){
 
@@ -124,16 +195,16 @@ window.onload = function (){
   document.getElementById("nopeButton").addEventListener("click", fail);
   document.getElementById("superButton").addEventListener("click", saveColor);
   document.getElementById("resetButton").addEventListener("click", resetBeta);
+  document.getElementById("shadeButton").addEventListener("click", toggleShades);
+  setClassEventListener("shade", "click", moveToSample);
 
-  document.getElementById("infoButton").addEventListener("click", (function () {alert("Use the Like and Nope button to find a color you like. \nYou can save a color to your palette by clicking the save button. \nThe reset button restarts the color selection path.")}));
-  
-  document.getElementById("exportPaletteButton").addEventListener("click", (function() {alert("" +
-    document.getElementById("palettePos0").style.backgroundColor + "\n" +
-    document.getElementById("palettePos1").style.backgroundColor + "\n" +
-    document.getElementById("palettePos2").style.backgroundColor + "\n" +
-    document.getElementById("palettePos3").style.backgroundColor + "\n" +
-    document.getElementById("palettePos4").style.backgroundColor + "");
-  }));
+
+  document.getElementById("infoButton").addEventListener("click", (function () {alert("Use the Like and Nope button to find a color you like.\n" + 
+    "You can save a color to your palette by clicking the save button.\n" +  
+    "The reset button restarts the color selection path.")}));
+
+  document.getElementById("exportPaletteButton").addEventListener("click", exportPalette);
+
   };
 
 return{
@@ -141,7 +212,10 @@ return{
   success:success,
   fail:fail,
   saveColor:saveColor,
-  resetBeta:resetBeta
+  resetBeta:resetBeta,
+  toggleShades:toggleShades,
+  moveToSample:moveToSample,
+  exportPalette:exportPalette,
 
 }
 }();
